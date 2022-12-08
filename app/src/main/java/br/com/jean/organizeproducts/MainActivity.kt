90d1aaa1a04289a93721ece25b5afc24222d011e
JeanFerreira <jeangabrielferreira1@gmail.com>
2022-12-08 16:31:51 -0300

    Iniciado criação do layout de lista de produtos

--- app/src/main/java/br/com/jean/organizeproducts/MainActivity.kt
+++ app/src/main/java/br/com/jean/organizeproducts/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.jean.organizeproducts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.jean.organizeproducts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
